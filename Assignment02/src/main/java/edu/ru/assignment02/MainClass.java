c259afcc42a45a0d6902ddc78c41f7698bc2b2ef
Maria Palacios <mariapalacios5889@gmail.com>
2021-02-22 13:26:07 -0600

    added project code

--- Assignment02/src/main/java/edu/ru/assignment02/MainClass.java
+++ Assignment02/src/main/java/edu/ru/assignment02/MainClass.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.ru.assignment02;
 
-/**
- *
- * @author mac
- */
 public class MainClass {
-    
+
+    public static void main(String[] args) {
+
+    int result = addAllDigits(638);
+        System.out.println(result);
+
+    }
+
+    public static int addAllDigits(int inputNumber) {
+
+        int num = inputNumber % 10;
+        int num2 = inputNumber / 10;
+        int num3 = num2 % 10;
+        int num4 = num2 / 10;
+        
+       
+        int result = num + num3  +num4;
+        System.out.println(num + " " + num2 + " " + num3 + " " + num4);
+        return result;
+    }
 }
